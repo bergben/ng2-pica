4aa2037f244d7ff2b76e5ab08b2a045a352ba9d5
Benedikt Berger <benedikt.berger@live.de>
2016-12-10 18:59:57 +0100

    provider for service added

--- src/ng2-pica.module.ts
+++ src/ng2-pica.module.ts
@@ -2,16 +2,8 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { Ng2PicaService } from './ng2-pica.service';
 
 @NgModule({
-  providers: [Ng2PicaService]
-})
-export class Ng2PicaModule {
-  static forRoot(): ModuleWithProviders {
-    return {
-      ngModule: Ng2PicaModule,
-      providers: [
+    providers: [
         {provide: Ng2PicaService, useClass: Ng2PicaService}
-      ]
-    };
-
-  }
-}
+    ]
+})
+export class Ng2PicaModule {}
