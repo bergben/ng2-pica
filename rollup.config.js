e68a2d52ba8ccad0f199072cbb081d1a5c8bb016
Berger Benedikt <benedikt.berger@live.de>
2017-02-20 20:03:54 +0100

    Create rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,11 @@
+export default {
+  entry: 'dist/ng2-pica.js',
+  dest: 'dist/bundles/ng2-pica.umd.js',
+  sourceMap: false,
+  format: 'umd',
+  moduleName: 'ng2-pica',
+  globals: {
+    '@angular/core': 'ng.core',
+    'rxjs': 'Rx'
+  }
+}
